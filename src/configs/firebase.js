d76601b317be9abf5384394352eae38db642e35b
Gabriela Ciurea <gabrielaciurea19@gmail.com>
2021-05-23 19:13:53 +0300

    initialiaze project

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCcbZMkwd4ZV_1DiH0R3TZFCg9GKaIMeoM",
-    authDomain: "react-sirluggia-shop.firebaseapp.com",
-    databaseURL: "https://react-sirluggia-shop.firebaseio.com",
-    projectId: "react-sirluggia-shop",
-    storageBucket: "react-sirluggia-shop.appspot.com",
-    messagingSenderId: "184253035461",
-    appId: "1:184253035461:web:20fe5c173c7675c74c51ac"
-};
+    apiKey: "AIzaSyBTFOT9aRKdtyyojGCtxO_JUGF7lDqsBoE",
+    authDomain: "curs07.firebaseapp.com",
+    projectId: "curs07",
+    storageBucket: "curs07.appspot.com",
+    messagingSenderId: "1055397223732",
+    appId: "1:1055397223732:web:cc0e08bb50770233f1d961"
+  };
 
 export default firebaseConfig;
